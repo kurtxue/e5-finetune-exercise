23c14c1c951b7dcea2a019d12619c17f275d8825
kurtxue <kurt.xue@mail.utoronto.ca>
2024-05-04 16:20:07 -0400

    fix some bugs

--- main.py
+++ main.py
@@ -6,50 +6,64 @@ from transformers import AutoTokenizer, AutoModel
 
 from tqdm import tqdm
 import pandas as pd
-from datasets import load_dataset
-from datasets import Dataset
+from datasets import load_dataset_builder
+from torch.utils.data import Dataset
+
 import numpy as np
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
 
 import matplotlib.pyplot as plt
 
-
+class MyDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+        
+    def __len__(self):
+        return len(self.data['query'])
+    
+    def __getitem__(self, idx):
+        return {key: self.data[key][idx] for key in self.data.keys()}
 
 def load_data(path, batch_size=32):
     #positive_samples
-    qrels_dev = pd.read_json(path+'qrels_dev.jsonl', lines=True)
-    qrels_test = pd.read_json(path+'qrels_test.jsonl', lines=True)
+    qrels_dev = pd.read_json(path+'qrels_dev.jsonl', lines=True).reset_index(drop=True)
+    qrels_test = pd.read_json(path+'qrels_test.jsonl', lines=True).reset_index(drop=True)
     positive_samples = pd.concat([qrels_dev, qrels_test], ignore_index=True)
     
-    corpus = pd.read_json(path+'corpus.jsonl', lines=True)
-    queries = pd.read_json(path+'queries.jsonl', lines=True)
+    corpus = pd.read_json(path+'corpus.jsonl', lines=True).reset_index(drop=True)
+    queries = pd.read_json(path+'queries.jsonl', lines=True).reset_index(drop=True)
     
-    corpus_id_to_index = {corpus_id: index for index, corpus_id in enumerate(corpus['corpus_id'])}
-    index_to_corpus_id = {index: corpus_id for index, corpus_id in enumerate(corpus['corpus_id'])}
-    query_id_to_index = {query_id: index for index, query_id in enumerate(queries['query_id'])}
-    index_to_query_id = {index: query_id for index, query_id in enumerate(queries['query_id'])}
-    true_scores = [0 for _ in range(len(corpus)) for _ in range(len(queries))]
-    for index, row in positive_samples.iterrows():
-        query_id = row['query_id']
-        corpus_id = row['corpus_id']
-        true_scores[query_id_to_index[query_id]][corpus_id_to_index[corpus_id]] = row['score']
-    dataset = {'query': [queries['query'][query_id_to_index[query_id]] for query_id in positive_samples['query_id']], 'corpus': [corpus['corpus'][corpus_id_to_index[corpus_id]] for corpus_id in positive_samples['corpus_id']], 
-               'query_id': positive_samples['query_id'], 'corpus_id': positive_samples['corpus_id'], 'score': positive_samples['score']}
+    corpus_id_to_index = {corpus_id: index for index, corpus_id in enumerate(corpus['_id'])}
+    index_to_corpus_id = {index: corpus_id for index, corpus_id in enumerate(corpus['_id'])}
+    query_id_to_index = {query_id: index for index, query_id in enumerate(queries['_id'])}
+    index_to_query_id = {index: query_id for index, query_id in enumerate(queries['_id'])}
+    dataset = {'query': [queries['text'][query_id_to_index[query_id]] for query_id in positive_samples['query-id']], 'corpus': [corpus['text'][corpus_id_to_index[corpus_id]] for corpus_id in positive_samples['corpus-id']], 
+               'query_id': positive_samples['query-id'], 'corpus_id': positive_samples['corpus-id']}
+
     
-    train_dataset, test_dataset = train_test_split(dataset, test_size=0.2)
-    test_dataset, val_dataset = train_test_split(test_dataset, test_size=0.5)
+    train_query, test_query, train_corpus, test_corpus, train_query_id, test_query_id, train_corpus_id, test_corpus_id = train_test_split(
+       dataset['query'], dataset['corpus'], dataset['query_id'].tolist(), dataset['corpus_id'].tolist(), test_size=0.2)
+
+    train_dataset = MyDataset({'query': train_query, 'corpus': train_corpus, 'query_id': train_query_id, 'corpus_id': train_corpus_id})
+    test_query, val_query, test_corpus, val_corpus, test_query_id, val_query_id, test_corpus_id, val_corpus_id = train_test_split(
+         test_query, test_corpus, test_query_id, test_corpus_id, test_size=0.5)
     
+    test_dataset = MyDataset({'query': test_query, 'corpus': test_corpus, 'query_id': test_query_id, 'corpus_id': test_corpus_id})
+    val_dataset = MyDataset({'query': val_query, 'corpus': val_corpus, 'query_id': val_query_id, 'corpus_id': val_corpus_id})
+
+
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-    return dataset, positive_samples, train_loader, val_loader, test_loader, true_scores, query_id_to_index
 
-def tokenize_data(data, tokenizer):
-    queries = data['query'].unique()   
-    corpus = data['corpus'].unique()
+    return dataset, positive_samples, train_loader, val_loader, test_loader, query_id_to_index, corpus_id_to_index, train_dataset, val_dataset, test_dataset
+
+def tokenize_data(data, tokenizer, query_id_to_index, corpus_id_to_index, device):
+    queries = list(set(data['query']))
+    corpus = list(set(data['corpus']))
     input_texts = prepend_prefix(queries, corpus)
-    embeddings = tokenizer(input_texts, max_length=512, padding=True, truncation=True, return_tensors='pt')
+    embeddings = tokenizer(input_texts, max_length=512, padding=True, truncation=True, return_tensors='pt').to(device)
     return embeddings
     
 def generate_negative_samples(batch):
@@ -57,29 +71,36 @@ def generate_negative_samples(batch):
     positive_index = []
     queries = batch['query_id'].unique()
     corpuses = batch['corpus_id'].unique()
-   
-    for row in enumerate(batch):
-        index
+    
+    for i in range(len(batch['query_id'])):
+        index = [0, 0]
         negative_q = []
         negative_c = []
-        query = row['query_id']
-        corpus = row['corpus_id']
+        query = batch['query_id'][i]
+        corpus = batch['corpus_id'][i]
         unrelated_samples = {'query_id': [], 'corpus_id': []}
-        for row in enumerate(batch):
-            if row['query_id'] != query and row['corpus_id'] != corpus:
-                unrelated_samples['query_id'].append(row['query_id'])
-                unrelated_samples['corpus_id'].append(row['corpus_id'])
+        for j,_ in enumerate(batch):
+            if batch['query_id'][j] != query:
+                unrelated_samples['query_id'].append(batch['query_id'][j])
+            if batch['corpus_id'][j] != corpus:
+                unrelated_samples['corpus_id'].append(batch['corpus_id'][j])
         for x, q in enumerate(queries):
-            if q != query and q not in unrelated_samples['query_id']:
+            if q != query and q in unrelated_samples['query_id']:
                 negative_c.append(x)
-            for y, c in enumerate(corpuses):
-                if  c != corpus and c not in unrelated_samples['corpus_id']:
-                    negative_q.append(y)
-                if q == query and c == corpus:
-                    index = (x, y)
-        positive_index.append(index)
+            if q == query:
+                index[0] = x
+        for y, c in enumerate(corpuses):
+            if  c != corpus and c in unrelated_samples['corpus_id']:
+                negative_q.append(y)
+            if  c == corpus:
+                index[1] = y
+        positive_index.append(tuple(index))
+        if negative_c == []:
+            negative_index['negative_c'].append(0)
         negative_index['negative_q'].append([(index[0], i) for i in negative_q])
         negative_index['negative_c'].append([(i, index[1]) for i in negative_c])
+        
+
                     
     return positive_index, negative_index, queries, corpuses
 
@@ -89,7 +110,7 @@ def average_pool(last_hidden_states: Tensor,
     return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
 
 
-def train(model, tokenizer, data, train_loader, val_loader, num_epochs, learning_rate, patience, true_scores, query_id_to_index, device):
+def train(model, tokenizer, data, train_loader, val_loader, train_data, val_data, num_epochs, learning_rate, patience, query_id_to_index, corpus_id_to_index, device):
     model.train()
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
     train_loss, train_acc, val_loss, val_acc = [], [], [], []
@@ -100,9 +121,8 @@ def train(model, tokenizer, data, train_loader, val_loader, num_epochs, learning
         total_loss = 0
         for batch in tqdm(train_loader):
             optimizer.zero_grad()
-            batch.to(device)
             positive_index, negative_index, queries, corpuses = generate_negative_samples(batch)
-            inputs = prepend_prefix(queries, corpuses)
+            inputs = tokenize_data(batch, tokenizer, query_id_to_index, corpus_id_to_index, device)
             n_queries = len(queries)
             n_corpuses = len(corpuses)
             inputs_ids = inputs['input_ids']
@@ -117,10 +137,10 @@ def train(model, tokenizer, data, train_loader, val_loader, num_epochs, learning
             optimizer.step()
         
         avg_loss = total_loss / len(train_loader)
-        t_acc = accuracy(train_loader, true_scores, query_id_to_index)
+        t_acc = accuracy(train_data, model, tokenizer, query_id_to_index, corpus_id_to_index, device)
         train_loss.append(float(avg_loss))
         train_acc.append(float(t_acc))
-        v_loss, v_acc = evaluate(model, val_loader, true_scores, query_id_to_index)
+        v_loss, v_acc = evaluate(model, val_loader, val_data, query_id_to_index, corpus_id_to_index, device)
         val_loss.append(float(v_loss))
         val_acc.append(float(v_acc))
         print(f'End of Epoch {epoch}, Training Loss: {avg_loss}, Training Accuracy: {t_acc}, Validation Loss: {v_loss}, Validation Accuracy: {v_acc}')
@@ -140,14 +160,13 @@ def train(model, tokenizer, data, train_loader, val_loader, num_epochs, learning
     return train_loss, train_acc, val_loss, val_acc
 
 
-def evaluate(model, test_loader, true_scores, query_id_to_index):
+def evaluate(model, test_loader, test_data, query_id_to_index, corpus_id_to_index, device):
     model.eval()
     test_loss = 0
     with torch.no_grad():
         for batch in test_loader:
-            batch.to(device)
             positive_index, negative_index, queries, corpuses = generate_negative_samples(batch)
-            inputs = prepend_prefix(queries, corpuses)
+            inputs = tokenize_data(batch, tokenizer, query_id_to_index, corpus_id_to_index, device)
             n_queries = len(queries)
             n_corpuses = len(corpuses)
             inputs_ids = inputs['input_ids']
@@ -159,7 +178,7 @@ def evaluate(model, test_loader, true_scores, query_id_to_index):
             loss = compute_loss(scores_matrix, positive_index, negative_index)
             test_loss += loss.item()
     avg_loss = test_loss / len(test_loader)
-    acc = accuracy(test_loader, true_scores, query_id_to_index)
+    acc = accuracy(test_data, model, tokenizer, query_id_to_index, corpus_id_to_index, device)
     
     return avg_loss, acc
 
@@ -172,30 +191,41 @@ def prepend_prefix(queries, corpuses):
     
 def compute_loss(scores_matrix, positive_index, negative_index):
     total_loss = 0
-    for i, (pos, neg) in enumerate(zip(positive_index, negative_index)):
-        loss_q = -torch.log(torch.exp(scores_matrix[pos]) / (torch.exp(scores_matrix[pos]) + torch.sum(torch.exp(scores_matrix[neg['negative_q']]))))
-        loss_c = -torch.log(torch.exp(scores_matrix[pos]) / (torch.exp(scores_matrix[pos]) + torch.sum(torch.exp(scores_matrix[neg['negative_c']]))))
+    for pos, neg_q, neg_c in zip(positive_index, negative_index['negative_q'], negative_index['negative_c']):
+        q_rows, q_cols = zip(*neg_q)
+    
+        loss_q = -torch.log(torch.exp(scores_matrix[pos]) / (torch.exp(scores_matrix[pos]) + torch.sum(torch.exp(scores_matrix[q_rows, q_cols]))))
+        if neg_c == 0:
+            loss_c = 0   
+        else:
+            c_rows, c_cols = zip(*neg_c)
+            loss_c = -torch.log(torch.exp(scores_matrix[pos]) / (torch.exp(scores_matrix[pos]) + torch.sum(torch.exp(scores_matrix[c_rows, c_cols]))))
         total_loss += loss_q + loss_c
     avg_loss = total_loss / len(positive_index)
     return avg_loss
     
     
-def accuracy(data, true_scores, query_id_to_index):
+def accuracy(data, model, tokenizer, query_id_to_index, corpus_id_to_index, device):
     total_score = 0
-    input = prepend_prefix(data['query'], data['corpus'])
-    
-    embeddings = model(input)
-    embeddings = average_pool(embeddings.last_hidden_state, data['attention_mask'])
-    embeddings = F.normalize(embeddings, p=2, dim=1)
-    predicted_scores = (embeddings[:2] @ embeddings[2:].T) * 100
     queries = data['query_id'].unique()
+    n_queries = len(queries)
+    inputs = tokenize_data(data, tokenizer, query_id_to_index, corpus_id_to_index, device)
+    inputs_ids = inputs['input_ids']
+    attention_mask = inputs['attention_mask']
+    outputs = model(inputs_ids)
+    embeddings = average_pool(outputs.last_hidden_state, attention_mask)
+    embeddings = F.normalize(embeddings, p=2, dim=1)
+    scores_matrix = (embeddings[:n_queries] @ embeddings[n_queries:].T) * 100
     
     for query in queries:
         query_index = query_id_to_index[query]
-        predicted_top10_indices = torch.argsort(predicted_scores[query_index], descending=True)[:10]
-        true_top10_indices = torch.argsort(true_scores[query_index], descending=True)[:10]
-        dcg = np.sum( (2 ** predicted_scores[query_index][predicted_top10_indices] - 1) / np.log2(np.arange(2, 12)))
-        idcg = np.sum( (2 ** true_scores[query_index][true_top10_indices] - 1) / np.log2(np.arange(2, 12)))
+        predicted_top10_indices = torch.argsort(scores_matrix[query_index], descending=True)[:10]
+        df = pd.DataFrame(data)
+        true_related = df[df['query_id'] == query].shape[0]
+        true_top10_score = [1] * true_related + [0] * 10 
+        true_top10_score = true_top10_score[:10]
+        dcg = np.sum( (2 ** scores_matrix[query_index][predicted_top10_indices] - 1) / np.log2(np.arange(2, 12)))
+        idcg = np.sum( (2 ** true_top10_score - 1) / np.log2(np.arange(2, 12)))
         ndcg = dcg / idcg
         total_score += ndcg
     score = total_score / len(queries)
@@ -236,29 +266,28 @@ if __name__ == '__main__':
     print("Current Device: {}".format(device))
     
     epochs = 10
-    learning_rate = 1e-5
+    learning_rate = 1e-4
     batch_size = 32
     
     
     
     data_path = 'Data/'
-    dataset, positive_samples, train_loader, val_loader, test_loader, true_scores, query_id_to_index = load_data(data_path, batch_size)
+    dataset, positive_samples, train_loader, val_loader, test_loader, query_id_to_index, corpus_id_to_index, train_data, val_data, test_data = load_data(data_path, batch_size)
     
     tokenizer = AutoTokenizer.from_pretrained('intfloat/e5-base-v2')
     model = AutoModel.from_pretrained('intfloat/e5-base-v2')
     
     model.to(device)
-    tokenizer.to(device)
     
     print("Starting Training")
     
-    train_loss, train_acc, val_loss, val_acc = train(model, tokenizer, dataset, train_loader, val_loader, epochs, learning_rate, 3, true_scores, query_id_to_index, device)
+    train_loss, train_acc, val_loss, val_acc = train(model, tokenizer, dataset, train_loader, val_loader, train_data, val_data, epochs, learning_rate, 10, query_id_to_index, corpus_id_to_index, device)
     
     plot_loss(train_loss, train_acc, val_loss, val_acc)
     
     print("Starting Evaluation")
     
-    loss, acc = evaluate(model, test_loader, true_scores, query_id_to_index)
+    loss, acc = evaluate(model, test_loader, test_data, query_id_to_index, corpus_id_to_index, device)
     
     print(f"Average Test Loss: {loss}, Test Accuracy: {acc}")
     
